c24343dd547ce1cf035375cba25ee8a7e49ccefc
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-19 15:45:46 +0200

    Create index.js

---
+++ data/index.js
@@ -0,0 +1,54 @@
+export const slide = [
+    {
+        id: 1,
+        name: "DEZEMBRO",
+        discription: "Discription for this food broh",
+        image: require('../assets/imgs/slides/slide1.jpg'),
+    }, {
+        id: 2,
+        name: "Novembro",
+        discription: "Discription for this food broh",
+        image: require('../assets/imgs/slides/slide2.jpg'),
+    }
+]
+
+export const Nearest = [
+    {
+        id: 1,
+        name: "Marlon Food",
+        min: "25 min",
+        image: require('../assets/imgs/Resturant/Restaurant1.png'),
+    },
+    {
+        id: 2,
+        name: "King Burguer",
+        min: "10 mim",
+        image: require('../assets/imgs/Resturant/Restaurant2.png'),
+    },
+    {
+        id: 3,
+        name: "Casa da pizzas",
+        min: "1 Hours",
+        image: require('../assets/imgs/Resturant/Restaurant3.png'),
+    }
+]
+export const menu = [
+    {
+        id: 1,
+        name: "Unsplash",
+        price: "25.85",
+        image: require('../assets/imgs/menu/menu1.jpg'),
+    },
+    {
+        id: 2,
+        name: "key={data.id}",
+        price: "48,66",
+        image: require('../assets/imgs/menu/menu2.jpg'),
+    },
+    {
+        id: 3,
+        name: "Chicken Pizza Pictures",
+        price: "58,000",
+        image: require('../assets/imgs/menu/menu3.jpg'),
+    }
+]
